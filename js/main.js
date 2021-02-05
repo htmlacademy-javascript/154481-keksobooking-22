fa3d34c4d23c05aca23e6a4f41dbcb0e2e877448
Maria <afterlifelineonomega@gmail.com>
2021-02-05 21:38:00 +0300

    Рефакторит код функций получения рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const getRandomNumber = function (min, max) {
-  if (min < 0) {
-    min = Math.abs(min);
-  }
-
-  if (max < 0) {
-    max = Math.abs(max);
-  }
-
-  if (min > max) {
-    let minValueStorage = min;
-    min = max;
-    max = minValueStorage;
-  }
-
-  let randomNumber = Math.round(Math.random() * (max - min) + min);
-
-  return randomNumber;
-}
-
 const getRandomFloatNumber = function (min, max, simbolsNumberAfterPoint) {
   if (min < 0) {
     min = Math.abs(min);
@@ -34,11 +14,18 @@ const getRandomFloatNumber = function (min, max, simbolsNumberAfterPoint) {
   }
 
   let randomFloatNumber = Math.random() * (max - min) + min;
-  let randomFloatNumberFixed = randomFloatNumber.toFixed(simbolsNumberAfterPoint);
 
-  return randomFloatNumberFixed;
+  if (simbolsNumberAfterPoint) {
+    let randomFloatNumberFixed = randomFloatNumber.toFixed(simbolsNumberAfterPoint);
+    return randomFloatNumberFixed;
+  }
+
+  return randomFloatNumber;
+}
+
+const getRandomNumber = function (min, max) {
+  return Math.round(getRandomFloatNumber(min, max));
 }
 
 getRandomNumber();
 getRandomFloatNumber();
-
